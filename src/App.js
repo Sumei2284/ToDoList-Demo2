553cbccba8049cbe92648895f9204308ef3e7ff5
Sumei2284 <x21282@gmail.com>
2021-11-12 00:56:31 +0800

    create basic todo UI and functions by YT tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Form from './components/Form.js';
+import TodoList from "./components/TodoList";
 
 function App() {
+  //State stuff
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setfilteredTodos] = useState([]);
+
+  //Use Effect
+  useEffect(() => {
+    filterHandler();
+  }, [todos,status]);
+  //Functions
+  //when typing new strings or changing the option (2 situations to run this function)
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setfilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+        case 'in progress':
+          setfilteredTodos(todos.filter(todo => todo.completed === false));
+          break;
+        default:
+          setfilteredTodos(todos);
+          break;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Todo List</h1>
       </header>
+      <Form 
+        todos={todos} 
+        setTodos={setTodos} 
+        inputText={inputText} 
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList 
+        setTodos={setTodos} 
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
